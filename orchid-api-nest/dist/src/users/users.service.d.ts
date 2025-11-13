bb56882fefcd481aa6a8590a0e1a3c1e51d026ed
Awad Reda <awadreda496@gmail.com>
2025-11-13 17:23:47 +0200

    fix: ignore node_modules and build outputs in subfolders

--- orchid-api-nest/dist/src/users/users.service.d.ts
+++
@@ -1,7 +0,0 @@
-import { PrismaService } from 'src/prisma/prisma.service';
-import { User } from '@prisma/client';
-export declare class UsersService {
-    private readonly _prisma;
-    constructor(_prisma: PrismaService);
-    getUsers(): Promise<User[]>;
-}
