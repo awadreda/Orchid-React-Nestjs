0c3a6aae95df1ce1fd9243e713ed11426f5fde85
Awad Reda <awadreda496@gmail.com>
2025-11-15 16:43:44 +0200

    implement user feature cured with in controller and service

--- orchid-api-nest/src/users/users.controller.ts
+++ orchid-api-nest/src/users/users.controller.ts
@@ -6,17 +6,19 @@ import {
   Patch,
   Param,
   Delete,
+  Put,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { UserResponseDto } from './dto/user-response.dto';
 
-@Controller('users')
+@Controller('user')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Get()
-  async GetAllUsers() {
+  async GetAllUsers() : Promise<UserResponseDto[] | { message: string }> {
     const users = await this.usersService.getUsers();
 
     if (users.length === 0) {
@@ -24,4 +26,80 @@ export class UsersController {
     }
     return users;
   }
+
+    @Get('userbyid/:id')
+    async GetUserById(@Param('id') id:string): Promise<UserResponseDto | { message: string }>{
+
+   if(isNaN(parseInt(id))) {
+        return { message: 'Invalid user ID' };
+   }
+
+    const idNum = parseInt(id);
+      const user = await this.usersService.getUserById(idNum);
+
+    if(!user)
+    {
+        return { message: 'User not found' };
+    }
+
+    return user;
+
+
+    }
+
+
+
+      @Post('createuser')
+    async CreateUser(@Body() createUserDto: CreateUserDto): Promise<UserResponseDto | { message: string }>{
+        const user = await this.usersService.createUser(createUserDto);
+        if(!user){
+            return { message: 'User not created' };
+        }
+        return user;
+    }
+
+
+
+
+
+    @Put('updateuser/:id')
+    async UpdateUser(@Param('id') id:string, @Body() updateUserDto: UpdateUserDto): Promise<UserResponseDto | { message: string }>{
+
+
+    if(isNaN(parseInt(id))) {
+          return { message: 'Invalid user ID' };
+
+    }
+
+    const idNum = parseInt(id);
+    const updatedUser = await this.usersService.updateUser(idNum, updateUserDto);
+
+    if(!updatedUser){
+        return { message: 'User not found' };
+    }
+    return updatedUser;
+
+    }
+
+    @Delete('deleteuser/:id')
+    async DeleteUser(@Param('id') id:string): Promise< { message: string }>{
+    if(isNaN(parseInt(id))) {
+          return { message: 'Invalid user ID' };
+    }
+    const idNum = parseInt(id);
+    const result = await this.usersService.deleteUser(idNum);
+
+    if(!result){
+        return { message: 'User not found' };
+    }
+    return { message: 'User deleted successfully' };
+    
+    }
+
+
+      
+
+
+
+
 }
--- orchid-api-nest/src/users/users.service.ts
+++ orchid-api-nest/src/users/users.service.ts
@@ -3,32 +3,106 @@ import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { User } from '@prisma/client';
+import { UserResponseDto } from './dto/user-response.dto';
+import { UserMapper } from './Mapper/user-mapper';
 
 @Injectable()
 export class UsersService {
-  constructor(private readonly _prisma: PrismaService) {}
-  async getUsers(): Promise<User[]> {
+  constructor(private readonly _prisma: PrismaService
+      
+  ) {}
+
+
+   private userMapper = new UserMapper();
+
+  async getUsers(): Promise<UserResponseDto[]> {
     try {
       const users = await this._prisma.user.findMany();
 
-      return users;
+        const mappedUsers = this.userMapper.toResponseList(users);
+
+      return mappedUsers;
     } catch (error) {
       return [];
     }
   }
 
 
-  async getUserById(id:number) : Promise<User | null> {
+  async getUserById(id:number) : Promise<UserResponseDto   | null> {
     try {
       const user = await this._prisma.user.findUnique({
         where: { id },
       });
+      if (!user) {
+        return null;
+      }
 
-      return user;
+      return this.userMapper.toResponseDto(user);
     } catch (error) {
       return null;
     }
   }
+
+
+    async createUser(createUserDto:CreateUserDto) : Promise<UserResponseDto|null> {
+
+
+      try { 
+        const newUser = await this._prisma.user.create({
+          data:{
+            email:createUserDto.email,
+            name:createUserDto.name,
+            image:createUserDto.image
+            ,role:createUserDto.role,
+            createdAt:new Date(),
+
+            
+          }
+      
+        })
+        return this.userMapper.toResponseDto(newUser);
+      } catch (error) {
+        return null
+      }
+
+    }
+
+
+    async updateUser(id:number,updateUserDto:UpdateUserDto) : Promise<UserResponseDto|null> {
+      
+   
+      try {
+            const updatedUser = await this._prisma.user.update  ({
+              where:{id},
+              data:updateUserDto
+            })
+
+            if(!updatedUser){
+              return null;
+            }
+            return this.userMapper.toResponseDto(updatedUser);}
+        catch (error) { 
+          return null;
+
+
+      }
+   
+    }
+
+    async deleteUser(id:number) : Promise<boolean> {
+    
+    
+      try{
+        await this._prisma.user.delete({
+          where:{id}
+        });
+        return true;
+      } catch (error) {
+        return false;
+      }
+    }
+
+
 
 
     // async createUser(createUserDto: CreateUserDto): Promise<User> {
